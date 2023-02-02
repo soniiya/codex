f09f032d3ba3d460f2106e24bdfee4eb3a896835
soniiya <soniyajaiswal995@gmail.com>
2023-02-02 19:47:36 +0530

    initial

---
+++ client/script.js
@@ -0,0 +1,183 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(ele){
+  ele.textContainer='';
+
+  loadInterval=setInterval(()=>{
+    ele.textContent +='.';
+
+    if(ele.textContainer ==='....'){
+      ele.textContainer='';
+    }
+  },300);   //300 miliseconds
+}
+
+
+function typeText(ele,text){
+  let ind=0;
+
+  let interval=setInterval(()=>{
+    if(ind<text.length){
+    ele.innerHTML+=text.charAt(ind);
+    ind++;
+    }
+    else{
+      clearInterval(interval);
+    }
+  },20)   //in every 20 milisec
+}
+
+
+
+function generateUniqueId(){
+
+  const timestamp=Date.now();
+  const randomNumber= Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+      `
+      <div class="wrapper ${isAi && 'ai'}">
+          <div class="chat">
+              <div class="profile">
+                  <img 
+                    src=${isAi ? bot : user} 
+                    alt="${isAi ? 'bot' : 'user'}" 
+                  />
+              </div>
+              <div class="message" id=${uniqueId}>${value}</div>
+          </div>
+      </div>
+  `
+  )
+}
+
+
+// const handleSubmit = async(e)=>{
+//   e.preventDefault()
+
+//   const data= new FormData(form);
+
+//   //user's chatstripe
+//   chatContainer.innerHTML += chatStripe(false,data.get('prompt'));
+  
+//   form.reset();
+
+
+//   //bot's chatstripe
+//   const uniqueId=generateUniqueId();
+//   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+//   chatContainer.scrollTop = chatContainer.scrollHeight;
+
+//   const messageDiv = document.getElementById(uniqueId);
+
+//   loader(messageDiv);
+
+
+//   //fetch data from server (bot's response)
+//   const response = await fetch('http://localhost:5173/',{
+//     method: 'POST',
+//     headers:{
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       prompt: data.get('prompt')
+//     })
+//   })
+
+//   clearInterval(loadInterval);
+//   messageDiv.innerHTML=" ";
+
+//   if(response.ok){
+//     const data=await response.json();
+//     const parsedData = data.bot.trim();
+
+//     console.log(parsedData)
+
+//     typeText(messageDiv,parsedData);
+//   }
+//   else{
+//     const err = await response.text();
+//     messageDiv.innerHTML = 'Something went wrong';
+
+//     alert(err);
+//   }
+// }
+
+// form.addEventListener('submit',handleSubmit);
+// form.addEventListener('keyup', (e)=>{
+//   if(e.keyCode === 13){
+//     handleSubmit(e);
+//   }
+// })
+
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  // to clear the textarea input 
+  form.reset()
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+  // to focus scroll to the bottom 
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div 
+  const messageDiv = document.getElementById(uniqueId)
+
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv)
+
+  const response = await fetch('http://localhost:5000/', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          prompt: data.get('prompt')
+      })
+  })
+
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = " "
+
+  if (response.ok) {
+      const data = await response.json();
+      const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+      typeText(messageDiv, parsedData)
+  } else {
+      const err = await response.text()
+
+      messageDiv.innerHTML = "Something went wrong"
+      alert(err)
+  }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+      handleSubmit(e)
+  }
+})
