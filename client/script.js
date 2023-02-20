68091eb42a19463e502d61abdd229c5245e2b07f
soniiya <soniyajaiswal995@gmail.com>
2023-02-20 14:13:11 +0530

    update again

--- client/script.js
+++ client/script.js
@@ -149,7 +149,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('https://codexx-e31u.onrender.com', {
+  const response = await fetch('http://localhost:5000/', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
